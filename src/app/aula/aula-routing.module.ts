77f4eeab6e182c82b381b89eff90908312ceb734
EslinMakicine <eslinmartins@gmail.com>
2020-04-22 15:50:51 -0300

    removendo routes

--- src/app/aula/aula-routing.module.ts
+++ src/app/aula/aula-routing.module.ts
@@ -8,26 +8,6 @@ const routes: Routes = [
     path: "",
     component: AulaPage,
   },
-  {
-    path: "home",
-    loadChildren: () =>
-      import("../home/home.module").then((m) => m.HomePageModule),
-  },
-  {
-    path: "aulas",
-    loadChildren: () =>
-      import("../aulas/aulas.module").then((m) => m.AulasPageModule),
-  },
-  {
-    path: "aula",
-    loadChildren: () =>
-      import("../aula/aula.module").then((m) => m.AulaPageModule),
-  },
-  {
-    path: "turma",
-    loadChildren: () =>
-      import("../turma/turma.module").then((m) => m.TurmaPageModule),
-  },
 ];
 
 @NgModule({
